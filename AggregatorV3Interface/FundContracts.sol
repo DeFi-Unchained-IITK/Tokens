2d75baab26f8dacd5c9e798ea80309619c9a0d63
tusharsahu007 <135156507+tusharsahu007@users.noreply.github.com>
2024-06-26 15:13:00 +0530

    Update FundContracts.sol

--- AggregatorV3Interface/FundContracts.sol
+++ AggregatorV3Interface/FundContracts.sol
@@ -1,1 +1,98 @@
+// SPDX-License-Identifier: MIT
+
+
+// The contract does the following things:
+// 1. Makes the deployer of the contract the owner.
+// 2. Has fund() function to receive Ether from other accounts.
+// 3. Has withdraw() function only accessible to the owner. This function sends all Ether present in the contract to the owner's account.
+
+
+
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "/PriceConverter.sol";
+
+// interface AggregatorV3Interface {
+//   function decimals() external view returns (uint8);
+
+//   function description() external view returns (string memory);
+
+//   function version() external view returns (uint256);
+
+//   function getRoundData(
+//     uint80 _roundId
+//   ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+//   function latestRoundData()
+//     external
+//     view
+//     returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+// }
+
+
+error NotOwner();
+
+contract FundContract {
+
+
+   using PriceConverter for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+
+ 
+    address public immutable owner;
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to send more ETH!");
+        //require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to send more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == owner);
+        if (msg.sender != owner) revert NotOwner();
+        _;
+    }
+
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        //transfer
+        //payable(msg.sender).transfer(address(this).balance);
+
+        // // send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send failed");
+
+        // call
+        // (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        // require(callSuccess, "Call failed");
+    }
+
+
+
+    fallback() external payable {
+        fund();
+    }
+
+    receive() external payable {
+        fund();
+    }
+}
 
